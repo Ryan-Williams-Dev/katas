12cc9ec448fbe78e2a8992b5fe9eadd8a6197428
Ryan Williams <r.d.williams.ca@gmail.com>
2022-01-26 18:04:16 +0000

    #3 fixed

--- vowels.js
+++ vowels.js
@@ -1,5 +1,5 @@
 const numberOfVowels = function(data) {
-  total = 0;
+  let total = 0;
   vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
   for(let i = 0; i < data.length; i++) {
     for(let j = 0; j < vowels.length; j++) {
