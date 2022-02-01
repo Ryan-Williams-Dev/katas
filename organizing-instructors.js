4d125bdc738b269728d7f92dc9a2b9edb44bdd9d
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-01 20:09:38 +0000

    kata 15

---
+++ organizing-instructors.js
@@ -0,0 +1,25 @@
+const organizeInstructors = function(instructors) {
+  let output = {
+    
+  };
+  for (let i of instructors) {
+    if (!output[i.course]) {
+      output[i.course] = [];
+    }
+    output[i.course].push(i.name);
+  }
+  return output;
+};
+
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
+]));
