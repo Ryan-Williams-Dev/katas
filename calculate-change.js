424e937d10080e818ca571b3671f0367d6186cdd
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-01 18:28:20 +0000

    comepletely changed my methodology, had to look up a little help with solution

--- calculate-change.js
+++ calculate-change.js
@@ -1,52 +1,20 @@
 const calculateChange = function(total, cash) {
-  let changeLeft = cash - total;
-  let changeObject = {
-    twentyDollar: 0,
-    tenDollar: 0,
-    fiveDollar: 0,
-    twoDollar: 0,
-    oneDollar: 0,
-    quarter: 0,
-    dime: 0,
-    nickel: 0,
-    penny: 0
-  };
-  while (changeLeft > 0) {
-    if (changeLeft >= 2000) {
-      changeObject.twentyDollar += 1;
-      changeLeft -= 2000;
-    } else if (changeLeft >= 1000) {
-      changeObject.tenDollar += 1;
-      changeLeft -= 1000;
-    } else if (changeLeft >= 500) {
-      changeObject.fiveDollar += 1;
-      changeLeft -= 500;
-    } else if (changeLeft >= 200) {
-      changeObject.twoDollar += 1;
-      changeLeft -= 200;
-    } else if (changeLeft >= 100) {
-      changeObject.oneDollar += 1;
-      changeLeft -= 100;
-    } else if (changeLeft >= 25) {
-      changeObject.quarter += 1;
-      changeLeft -= 25;
-    } else if (changeLeft >= 10) {
-      changeObject.dime += 1;
-      changeLeft -= 10;
-    } else if (changeLeft >= 5) {
-      changeObject.nickel += 1;
-      changeLeft -= 5;
-    } else if (changeLeft >= 1) {
-      changeObject.penny += 1;
-      changeLeft -= 1;
+  let changeAmount = cash - total;
+  let change = {};
+
+  const coinDenoms = ['twentyDollar', 'tenDollar', 'fiveDollar', 'twoDollar', 'oneDollar', 'quarter', 'dime', 'nickel', 'penny'];
+  const coinValues = [2000, 1000, 500, 200, 100, 25, 10, 5, 1];
+
+  let amount;
+
+  for (let i = 0; i < coinValues.length; i++) {
+    amount = Math.floor(changeAmount / coinValues[i]);
+    if (amount > 0) {
+      change[coinDenoms[i]] = amount;
+      changeAmount = changeAmount % coinValues[i];
     }
   }
-  for (const property in changeObject) {
-    if (changeObject.property <= 0) {
-      delete changeObject.property;
-    }
-  }
-  return changeObject;
+  return change;
 };
 
 console.log(calculateChange(1787, 2000));
