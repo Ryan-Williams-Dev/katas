8cc263af22c90c42b6700e38e87cffb16e11db5a
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-03 22:31:53 +0000

    tried my hand at regular expressions. unsure if the JSON parse is cheating in this challenge or not but it was good to learn about regardless

---
+++ url-decode.js
@@ -0,0 +1,13 @@
+const urlDecode = function(text) {
+  let resultString = '{"' + decodeURI(text).replace(/=/g, '":"').replace(/&/g, '","') + '"}';
+  let resultObject = JSON.parse(resultString);
+  return resultObject;
+};
+
+console.log(urlDecode("duck=rubber"));
+console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
+
+/* Decided to take a shot at using regular expressions in this challenge.
+  They seem like a powerful tool */
